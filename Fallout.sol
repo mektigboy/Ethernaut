f383f8f6bf8d8b12d0b43987f5dd14614f97d51c
Ángel Sánchez <mektigboy@protonmail.com>
2022-07-04 21:14:16 -0500

    Update files

--- Fallout.sol
+++ Fallout.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./SafeMath.sol";
 
 contract Fallout {
     using SafeMath for uint256;
--- Reentrancy.sol
+++ Reentrancy.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./SafeMath.sol";
 
 contract Reentrancy {
     using SafeMath for uint256;
