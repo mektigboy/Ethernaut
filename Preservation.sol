97ac7b8fb72ae7d58c800d8d82099f20028ce9e5
Ángel Sánchez <mektigboy@protonmail.com>
2022-07-06 14:06:14 -0500

    Update files

--- Preservation.sol
+++ Preservation.sol
@@ -3,38 +3,38 @@ pragma solidity ^0.6.0;
 
 contract Preservation {
 
-  // public library contracts
-  address public timeZone1Library; // Slot 0
-  address public timeZone2Library; // Slot 1
-  address public owner; // Slot 2
-  uint storedTime; // Slot 3
-  // Sets the function signature for <delegatecall>.
-  bytes4 constant setTimeSignature = bytes4(keccak256("setTime(uint256)"));
+    // public library contracts
+    address public timeZone1Library; // Slot 0
+    address public timeZone2Library; // Slot 1
+    address public owner; // Slot 2
+    uint storedTime; // Slot 3
+    // Sets the function signature for <delegatecall>.
+    bytes4 constant setTimeSignature = bytes4(keccak256("setTime(uint256)"));
 
-  constructor(address _timeZone1LibraryAddress, address _timeZone2LibraryAddress) public {
-    timeZone1Library = _timeZone1LibraryAddress;
-    timeZone2Library = _timeZone2LibraryAddress;
-    owner = msg.sender;
-  }
+    constructor(address _timeZone1LibraryAddress, address _timeZone2LibraryAddress) public {
+        timeZone1Library = _timeZone1LibraryAddress;
+        timeZone2Library = _timeZone2LibraryAddress;
+        owner = msg.sender;
+    }
 
-  // Set the time for timezone 1.
-  function setFirstTime(uint _timeStamp) public {
-    timeZone1Library.delegatecall(abi.encodePacked(setTimeSignature, _timeStamp));
-  }
+    // Set the time for timezone 1.
+    function setFirstTime(uint _timeStamp) public {
+        timeZone1Library.delegatecall(abi.encodePacked(setTimeSignature, _timeStamp));
+    }
 
-  // Set the time for timezone 2.
-  function setSecondTime(uint _timeStamp) public {
-    timeZone2Library.delegatecall(abi.encodePacked(setTimeSignature, _timeStamp));
-  }
+    // Set the time for timezone 2.
+    function setSecondTime(uint _timeStamp) public {
+        timeZone2Library.delegatecall(abi.encodePacked(setTimeSignature, _timeStamp));
+    }
 }
 
 // Simple library contract to set the time.
 contract LibraryContract {
 
-  // Stores a timestamp.
-  uint storedTime;
+    // Stores a timestamp.
+    uint storedTime;
 
-  function setTime(uint _time) public {
-    storedTime = _time;
-  }
+    function setTime(uint _time) public {
+        storedTime = _time;
+    }
 }
