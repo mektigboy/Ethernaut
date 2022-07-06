3b5f6da336f2c79f68880af9e42538b6613a5ea2
Ángel Sánchez <mektigboy@protonmail.com>
2022-07-06 00:19:41 -0500

    Update files

--- NaughtCoin.sol
+++ NaughtCoin.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import './ERC20.sol';
+import "./ERC20.sol";
 
 contract NaughtCoin is ERC20 {
     // string public constant name = 'NaughtCoin';
