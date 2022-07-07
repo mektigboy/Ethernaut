37a8281729eb212d64c50b1b0c2ac54e809f4286
Ángel Sánchez <mektigboy@protonmail.com>
2022-07-06 22:37:27 -0500

    Update files

--- MagicNumber.js
+++
@@ -1,4 +0,0 @@
-var bytecode = "600a600c600039600a6000f3602a60505260206050f3";
-var txn = await web3.eth.sendTransaction({ from: player, data: bytecode });
-var solverAddress = txn.contractAddress;
-await contract.setSolver(solverAddress);
