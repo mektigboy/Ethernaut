266fbbc448d3cd486d3d5e49ab88b16e88add55f
Ángel Sánchez <mektigboy@protonmail.com>
2022-07-07 19:59:17 -0500

    Update files

--- DexTwo.sol
+++ DexTwo.sol
@@ -7,7 +7,7 @@ import '@openzeppelin/contracts/math/SafeMath.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract DexTwo is Ownable {
-    using SafeMath for uint;
+    using SafeMath for uint256;
     address public token1;
     address public token2;
 
@@ -18,40 +18,40 @@ contract DexTwo is Ownable {
         token2 = _token2;
     }
 
-    function add_liquidity(address token_address, uint amount) public onlyOwner {
+    function add_liquidity(address token_address, uint256 amount) public onlyOwner {
         IERC20(token_address).transferFrom(msg.sender, address(this), amount);
     }
 
-    function swap(address from, address to, uint amount) public {
+    function swap(address from, address to, uint256 amount) public {
         require(IERC20(from).balanceOf(msg.sender) >= amount, "Not enough to swap");
-        uint swapAmount = getSwapAmount(from, to, amount);
+        uint256 swapAmount = getSwapAmount(from, to, amount);
         IERC20(from).transferFrom(msg.sender, address(this), amount);
         IERC20(to).approve(address(this), swapAmount);
         IERC20(to).transferFrom(address(this), msg.sender, swapAmount);
     }
 
-    function getSwapAmount(address from, address to, uint amount) public view returns(uint){
+    function getSwapAmount(address from, address to, uint256 amount) public view returns(uint256){
         return((amount * IERC20(to).balanceOf(address(this)))/IERC20(from).balanceOf(address(this)));
     }
 
-    function approve(address spender, uint amount) public {
+    function approve(address spender, uint256 amount) public {
         SwappableTokenTwo(token1).approve(msg.sender, spender, amount);
         SwappableTokenTwo(token2).approve(msg.sender, spender, amount);
     }
 
-    function balanceOf(address token, address account) public view returns (uint){
+    function balanceOf(address token, address account) public view returns (uint256){
         return IERC20(token).balanceOf(account);
     }
 }
 
 contract SwappableTokenTwo is ERC20 {
     address private _dex;
-    constructor(address dexInstance, string memory name, string memory symbol, uint initialSupply) public ERC20(name, symbol) {
+    constructor(address dexInstance, string memory name, string memory symbol, uint256 initialSupply) public ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
         _dex = dexInstance;
     }
 
-    function approve(address owner, address spender, uint256 amount) public returns(bool){
+    function approve(address owner, address spender, uint256256 amount) public returns(bool){
         require(owner != _dex, "InvalidApprover");
         super._approve(owner, spender, amount);
     }
